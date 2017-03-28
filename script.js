fb1d3b2e65f4cc760ea6f4ef224868dc5da4f261
Jason Andrada <jpa323@nyu.edu>
2017-03-27 23:34:33 -0400

    getting somewhere, finally

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
   console.log('bitches');
   setTimeout(function() {
-    $('.name').addClass('magictime vanishIn');
+    $('#name').addClass('magictime vanishIn');
   }, 500);
+  const controller = new ScrollMagic.Controller();
+  const scene = new ScrollMagic.Scene({
+    triggerElement: '#name',
+    offset: 700, //start next section after 700px of scrolling
+    duration: 100
+  }).setPin('.about-me')
+  controller.addScene(scene);
+  const sceneTwo = new ScrollMagic.Scene({
+    triggerElement: '.about-me',
+    offset: 300,
+    duration: 100
+  }).setPin('.skills');
+  controller.addScene(sceneTwo);
 }); //end of document.ready
