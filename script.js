52752353e643a1a519a55e050309f8d45b7583a9
Jason Andrada <jpa323@nyu.edu>
2017-04-27 15:55:50 -0400

    fixed a small bug

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ $(document).ready(() => {
       $('.who-am-i').addClass('magictime vanishIn');
     }//end of vanishIn
     if($(window).scrollTop() > 1200){
-      $('.skills').addClass('magictime vanishIn');
+      $('.skill-showcase').addClass('magictime vanishIn');
     }
   })//end of scroll listener
 
   $('#header-one').hover(()=>{
     $('#description-one').toggleClass('open')
+  });
+  $('#header-two').hover(()=>{
+    $('#description-two').toggleClass('open')
+  });
+  $('#header-three').hover(()=>{
+    $('#description-three').toggleClass('open')
+  });
+  $('#header-four').hover(()=>{
+    $('#description-four').toggleClass('open')
   });
 
   // $('.project-header').hover(()=>{
