f342ab0c574835e57f16bf8eb6bc7b86697c533d
Jason Andrada <andrada.jasonp@gmail.com>
2025-04-07 15:48:17 -0400

    shrink hero text

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,7 +5,7 @@ export const Home = () => {
         <section id="home" className="min-h-screen flex items-center justify-center relative">
             <RevealOnScroll>
                 <div className="text-center z-10 px-4">
-                    <h1 className="text-4xl md:text-7xl font-bold mb-6 h-20 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right"> Product Engineer, Problem Solver, and Cat Lover</h1>
+                    <h1 className="text-xl md:text-5xl font-bold mb-6 h-20 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent leading-right"> Product Engineer, Problem Solver, and Cat Lover</h1>
                     <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">lorem ipsum something something</p>
                     <div className="flex justify-center space-x-4">
                         <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]"> View Projects</a>
