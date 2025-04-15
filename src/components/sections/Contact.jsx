85a8bd677778db6baeb24b6e7f2eea18c13a038b
Jason Andrada <andrada.jasonp@gmail.com>
2025-04-15 18:01:17 -0400

    change contact methods

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -3,15 +3,6 @@ import { useState } from "react";
 
 export const Contact = () => {
 
-    const [copied, setCopied] = useState(false);
-    const emailToClipboard = async () => {
-        try {
-            await navigator.clipboard.writeText("andrada.jasonp@gmail.com");
-            setCopied(true);
-        } catch (err) {
-            console.lerror("failed to copy:", err)
-        }
-    }
     return (
         <section id="contact" className="min-h-screen flex items-center justify-center py-20">
             <RevealOnScroll>
@@ -19,9 +10,8 @@ export const Contact = () => {
                 <h2 className="text-3xl font-bold mb-2 bg-gradient-to-r from-cyan-400 to-fuchsia-600 bg-clip-text text-transparent text-center">Get in Contact!</h2>
                 <div className="flex items-center justify-center py-2"></div>
                     <p className="text-gray-100 mb-6 text-center">
-                        Email me at <span onClick={() => emailToClipboard()} className="text-cyan-400 cursor-pointer underline">andrada.jasonp@gmail.com</span>
+                        Send me a message on <a href="https://www.linkedin.com/in/jason-andrada" target="_blank" className="text-cyan-400 cursor-pointer underline">LinkedIn</a>, or find me on <a href="https://www.github.com/jase-d-ace" target="_blank" className="text-cyan-400 cursor-pointer underline">GitHub</a>, or on Instagram at <a href="https://www.instagram.com/jase.d.ace" target="_blank" className="text-cyan-400 cursor-pointer underline">@jase.d.ace</a>
                     </p>
-                    <p className="text-center">My email address {copied ? "has been" : "will be"} copied to your clipboard for easy pasting into your email client!</p>
                 </div>
             </RevealOnScroll>
 
